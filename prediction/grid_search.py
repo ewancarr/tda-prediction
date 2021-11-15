7dbbf76beffbbb0df0c43f2fca065d30ec5516c9
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-11-15 12:37:28 +0000

    Many changes
    Compare included vs. excluded sample
    Adjust grid search parameters
    Refit 'best' models with repeated CV

--- prediction/grid_search.py
+++ prediction/grid_search.py
@@ -3,24 +3,42 @@
 # Started: 2021-06-25
 
 from pathlib import Path
+from datetime import datetime
 from joblib import load, dump
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import VarianceThreshold
-from sklearn.preprocessing import FunctionTransformer 
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.impute import KNNImputer
 from sklearn.manifold import MDS
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import (GridSearchCV,
+        RepeatedKFold, cross_validate)
 from glmnet import LogitNet
 import gudhi as gd
 from gudhi.representations import DiagramSelector, Landscape
+from scipy.stats import ttest_ind
+
+refit = False
+n_reps = 50
+latest = 'saved/2021_08_09/2021_08_08_153539_grid_search.joblib'
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                                                                           ┃
 # ┃                            Define functions                               ┃
 # ┃                                                                           ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+def cv_metric(fit, reps=50):
+    summary = {}
+    for k, v in fit.items():
+        if reps == 1:
+            summary[k] = np.mean(v)
+        else:
+            fold_means = [np.mean(i) for i in np.array_split(v, reps)]
+            summary[k] = np.percentile(fold_means, [50, 2, 98])
+            summary[k] = np.percentile(fold_means, [50])[0]
+    return(summary)
 
 def reshape(dat):
     var = dat.columns.str.startswith('rep_')
@@ -43,8 +61,9 @@ def knn(dat):
     dat = imp.fit_transform(dat)
     return(pd.DataFrame(dat, columns=cols, index=index))
 
+
 def compute_topological_variables(dat,
-                                  max_week=5,
+                                  max_week=6,
                                   mas=1e5,
                                   fun='landscape',
                                   dims=[0, 1, 2],
@@ -60,10 +79,12 @@ def compute_topological_variables(dat,
     for k in rv.index[~rv.index.duplicated()]:
         # Select this participant's rows
         # Reshape into grid of 'weeks' vs. 'measures'
+
         d = rv.loc[k, :]. \
             pivot(columns='var', values='value', index='w'). \
             loc[range(max_week + 1), :]. \
             values
+
         # Derive MDS components
         mds = MDS(n_components=3, random_state=42)
         d = mds.fit_transform(d.T)
@@ -90,13 +111,12 @@ def compute_topological_variables(dat,
     ls.set_axis(['X' + str(i) for i in ls.columns.values], axis=1, inplace=True)
 
     mrg = {'left_index': True, 'right_index': True, 'how': 'inner'}
-
     if keep_rm:
         # If we're keeping the repeated measures, first ensure we exclude
         # those measured beyond 'max_week'
         c = dat.columns[v]
         to_drop = np.array([])
-        for w in [i for i in range(0, 26) if i > max_week]:
+        for w in [i for i in range(0, 27) if i > max_week]:
             to_drop = np.concatenate([to_drop,
                                       c[c.str.contains('_w' + str(w) + '$')]])
         rep = dat.drop(labels=to_drop, axis=1)
@@ -116,7 +136,7 @@ def compute_topological_variables(dat,
 inp = Path('data')
 outcomes = load(inp / 'outcomes.joblib')
 baseline = load(inp / 'baseline.joblib')
-_, repwide = load(inp / 'repmea.joblib')
+replong, repwide = load(inp / 'repmea.joblib')
 
 # Add prefix to repeated measures to make it easier to identify later
 with_prefix = ['rep_' + i for i in repwide.columns]
@@ -125,14 +145,19 @@ repwide.columns = with_prefix
 # Merge into single WIDE dataset
 comb = baseline.merge(repwide, left_index=True, right_index=True, how='inner')
 comb = comb.loc[set(outcomes.index).intersection(comb.index), :]
+before = comb.copy()
+n1 = np.shape(comb)[0]
 
-# Remove people with at least 80% complete data among repeated measures
+
+# Remove people with less than 80% complete data among repeated measures
 # at weeks 0, 1 and 2. (Week 0 = baseline).
 r = comb.columns.str.contains('w[012]$')
 incl = ((comb.loc[:, r].notna().sum(axis=1) / r.sum()) > 0.80)
 pct = (comb.loc[:, r].notna().sum(axis=1) / r.sum())
 incl = pct[pct > 0.8].index
 comb = comb.loc[incl, :]
+after = comb.copy()
+n2 = np.shape(comb)[0]
 
 # Select outcome
 hdremit = outcomes.loc[incl, 'remit']
@@ -145,6 +170,7 @@ for o in opts:
     samp[o] = comb.loc[(comb['drug'] == o[1]) &
                        (comb['random'] == o[2]), ].index
 samp[('C', 'both', 'anyrandom')] = comb.index
+dump(samp, filename='samp.joblib')
 
 for k, v in samp.items():
     print(k, 'n =', len(v))
@@ -152,6 +178,40 @@ for k, v in samp.items():
 # Recode 'drug'; remove 'random'
 comb['escit'] = comb['drug'] == 'escitalopram'
 comb.drop(labels=['drug', 'random'], axis=1, inplace=True)
+
+# Recode 'drug'; remove 'random'
+baseline['escit'] = baseline['drug'] == 'escitalopram'
+baseline.drop(labels=['drug', 'random'], axis=1, inplace=True)
+
+
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                                           ┃
+# ┃                       Compare included vs. excluded                       ┃
+# ┃                                                                           ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+print(f'Sample size before any exclusions: {n1}')
+print(f'After excluding participants w/o repeated measures: {n2}')
+print(f'Number excluded: {n1 - n2}')
+
+before['incl'] = before.index.isin(after.index)
+
+def tt(var):
+    print(var, ttest_ind(before.loc[before['incl']][var],
+                         before.loc[~before['incl']][var],
+                         nan_policy='omit'),
+          '\n',
+          'incl:', np.mean(before.loc[before['incl']][var]),
+          '\n',
+          'excl:', np.mean(before.loc[~before['incl']][var]))
+
+tt('age')
+tt('educ')
+tt('madrs')
+tt('bdi')
+tt('hdrs')
+tt('bmi')
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                                                                           ┃
@@ -169,21 +229,18 @@ pipe = Pipeline(steps=[
                            random_state=42))])
 
 param_grid = []
-for land in [1, 3, 5, 10, 15]:
+for land in [3, 5, 10, 12, 15]:
     for bins in [10, 100, 1000, 2000]:
-        for mas in [10, 100, 1000, 2000, 1e5]:
+        for mas in [2000, 1e5]:
             for dims in [[0], [0, 1], [0, 1, 2]]:
-                for keep_rm in [True, False]:
-                    for alpha in [[0.5], [1.0]]:
-                        param_grid.append(
-                                {'topo__kw_args': [
-                                    {'fun': 'landscape',
+                param_grid.append({'topo__kw_args':
+                                   [{'fun': 'landscape',
                                      'n_land': land,
                                      'bins': bins,
                                      'dims': dims,
-                                     'mas': mas,
-                                     'keep_rm': keep_rm}],
-                                 'estimator__alpha': alpha})
+                                     'mas': mas}
+                                    ]
+                                   })
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                                                                           ┃
@@ -191,22 +248,134 @@ for land in [1, 3, 5, 10, 15]:
 # ┃                                                                           ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
-fit = {}
+if refit:
+    cv_inner = {}
+    for k, v in samp.items():
+        # For each sample [A, B, C]
+        for max_week in [2, 4, 6]:
+            # For increasing weeks of data
+            for keep_rm in [True, False]:
+                # With/without retaining the repeated measures in the model
+                params = param_grid.copy()
+                for p in params:
+                    p['topo__kw_args'][0]['keep_rm'] = keep_rm
+                    p['topo__kw_args'][0]['max_week'] = max_week
+                X = comb.loc[v].copy()
+                y = hdremit.loc[v].copy()
+                if k[1] != 'both':
+                    X.drop(labels=['escit'], axis=1, inplace=True)
+                gs = GridSearchCV(pipe,
+                                  param_grid,
+                                  cv=10,
+                                  scoring='roc_auc',
+                                  n_jobs=-1,
+                                  verbose=2)
+                cv_inner[(k, keep_rm, max_week)] = gs.fit(X, y)
+                del X
+                del y
+                del params
+        fn = (datetime.today().strftime('%Y_%m_%d_%H%M%S') +
+              '_grid_search.joblib')
+        dump(cv_inner, filename=fn)
+else:
+    cv_inner = load(latest)
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                                           ┃
+# ┃                 Refit 'best' parameters, with repeated CV                 ┃
+# ┃                                                                           ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+def evaluate_model(X, y, reps=50, impute=False):
+    if impute:
+        X = knn(X)
+    # Remove features with zero VarianceThreshold
+    vt = VarianceThreshold()
+    X = vt.fit_transform(X)
+    # Run repeated CV
+    clf = LogitNet(alpha=0.5,
+                   cut_point=0,
+                   n_splits=10,
+                   random_state=42)
+    rkf = RepeatedKFold(n_splits=10, n_repeats=reps, random_state=42)
+    fit = cross_validate(clf, X, y, cv=rkf, scoring='roc_auc', n_jobs=-1)
+    return(fit)
+
+cv_outer = {}
 for k, v in samp.items():
-    X = comb.loc[v].copy()
-    y = hdremit.loc[v].copy()
-    if k[1] != 'both':
-        X.drop(labels=['escit'], axis=1, inplace=True)
-    # cv = KFold(n_splits=5, shuffle=True, random_state=42)
-    gs = GridSearchCV(pipe,
-                      param_grid,
-                      cv=5,
-                      scoring='roc_auc',
-                      n_jobs=-1,
-                      verbose=10)
-    fit[k] = gs.fit(X, y)
-    del X
-    del y
+    for max_week in [2, 4, 6]:
+        # For increasing weeks of data
+        for keep_rm in [True, False]:
+            # Prepare X/y
+            X = comb.loc[v].copy()
+            y = hdremit.loc[v].copy()
+            if k[1] != 'both':
+                X.drop(labels=['escit'], axis=1, inplace=True)
+            # Get best parameters from inner CV
+            params = cv_inner[(k,
+                               keep_rm,
+                               max_week)].best_params_['topo__kw_args']
+            params['keep_rm'] = keep_rm
+            params['max_week'] = max_week
+            # Impute missing values
+            X = knn(X)
+            # Generate landscape variables (once, rather than at each CV iteration)
+            landscapes = compute_topological_variables(X, **params)
+            # Remove features with zero VarianceThreshold
+            vt = VarianceThreshold()
+            feat = vt.fit_transform(landscapes)
+            # Run repeated CV
+            cv_outer[(k,
+                      keep_rm,
+                      max_week)] = evaluate_model(landscapes, y, n_reps)
 
-dump(fit, filename='grid_search.joblib')
+for k, v in cv_outer.items():
+    print(k, v['test_score'].mean())
 
+fn = (datetime.today().strftime('%Y_%m_%d_%H%M%S') + '_outer_cv.joblib')
+dump(cv_outer, filename=fn)
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃                                                                           ┃
+# ┃           Fit models for growth curves / repeated measures only           ┃
+# ┃                                                                           ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+# Load growth curve parameters ------------------------------------------------
+alts = {}
+for mw in [2, 4, 6]:
+    g = load('prediction/re_params/re_' + str(mw))
+    g.columns = [i[0] + '_' + i[1].split('_')[1] for i in g.columns]
+    g = baseline.merge(g, left_index=True, right_index=True, how='inner')
+    alts['gc_' + str(mw)] = g
+
+# Define sets of 'raw' repeated measures --------------------------------------
+def reshape_rm(d, mw):
+    d = d[d['week'] <= mw].copy()
+    d['col'] = d['variable'] + '_w' + d['week'].astype('str')
+    return(d[['col', 'value']].pivot(columns='col', values='value'))
+
+for mw in [2, 4, 6]:
+    r = reshape_rm(replong, mw)
+    alts['rm_' + str(mw)] = baseline.merge(r,
+            left_index=True,
+            right_index=True,
+            how='inner')
+
+# Fit models for each ---------------------------------------------------------
+cv_alts = {}
+for k1, id in samp.items():
+    for k2, dat in alts.items():
+        # Prepare X/y
+        X = dat.loc[id].copy()
+        y = hdremit.loc[id].copy()
+        if k1[1] != 'both':
+            X.drop(labels=['escit'], axis=1, inplace=True)
+        # Run repeated CV
+        cv_alts[(k1, k2)] = evaluate_model(X, y, reps=n_reps, impute=True)
+
+for k, v in cv_alts.items():
+    print(k, v['test_score'].mean())
+
+fn = (datetime.today().strftime('%Y_%m_%d_%H%M%S') + '_outer_cv.joblib')
+dump([cv_outer, cv_alts], filename=fn)
